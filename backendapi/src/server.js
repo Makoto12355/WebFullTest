d6b031a9f9d663d0c16408edd6c0f1e27ba00ad6
Makoto12355 <juycf.001@atomicmail.io>
2026-07-01 12:03:47 +0700

    update 23

--- backendapi/src/server.js
+++ backendapi/src/server.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import categoryRoutes from "./routes/category.js";
 import menuRoutes from "./routes/menu.js";
 import tableRoutes from "./routes/table.js";
+import ordersRoutes from "./routes/orders.js";
+import orderItemRoutes from "./routes/order-item.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -20,6 +22,8 @@ app.get("/", (req, res) => {
 app.use("/api/categories", categoryRoutes);
 app.use("/api/menus", menuRoutes);
 app.use("/api/tables", tableRoutes);
+app.use("/api/orders", ordersRoutes);
+app.use("/api", orderItemRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
